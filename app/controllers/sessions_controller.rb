ea87c1b4529512620f5b56be5f386be27116f143
kimu805 <kimukimuok805@gmail.com>
2024-11-27 17:41:06 +0900

    add 2 actions of github and destroy to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def github
+    user_info = request.env["omniauth.auth"]
+
+    user = User.find_or_create_by(provider: user_info["provider"], uid: user_info["uid"]) do |u|
+      u.name = user_info["info"]["name"]
+      u.email = user_info["info"]["email"]
+    end
+
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "ログインしました！"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "ログアウトしました！"
+  end
+
 end
