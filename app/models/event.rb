2eb7be13718deeb99f9cfe2049ad580e3c47b211
kimu805 <kimukimuok805@gmail.com>
2024-11-29 17:48:42 +0900

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,20 @@
 class Event < ApplicationRecord
+  # validation
+  with_options presence:true do
+    validates :name, length: { maximum: 50 }
+    validates :place, length: { maximum: 100 }
+    validates :content, length: { maximum: 2000 }
+    validates :start_at
+    validates :end_at
+  end
+  validates :start_should_be_before_end
+
+  private
+  def start_should_be_before_end
+    return unless start_at && end_at
+
+    if start_at >= end_at
+      errors.add(:start_at, "は終了時間よりも前に設定してください")
+    end
+  end
 end
