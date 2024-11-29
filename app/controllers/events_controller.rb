ccaf34304e12048c93e54eff9d38876baffbed60
kimu805 <kimukimuok805@gmail.com>
2024-11-29 18:09:49 +0900

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,18 @@ class EventsController < ApplicationController
   def new
     @event = current_user.events.build
   end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: "「#{@event.name}」を作成しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :place, :content, :start_at, :end_at)
+  end
 end
