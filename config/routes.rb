583207fc905d71de7d59b7fc63c05477fa421714
kimu805 <kimukimuok805@gmail.com>
2024-11-29 17:53:03 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
+  # login/logout
   get "/auth/:provider/callback", to: "sessions#github"
   get "/auth/failure", to: redirect("/")
   delete "/logout", to: "sessions#destroy"
+
+  resources :events
 end
