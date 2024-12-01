ac88b854d8bec6c20d428ebd3b378150dfdb24b3
kimu805 <kimukimuok805@gmail.com>
2024-12-01 18:37:04 +0900

    Update tickets_controller.rb

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,19 @@
 class TicketsController < ApplicationController
+  def new
+    raise ActionController::Routingerror, "ログイン状態でTicketsController#newにアクセス"
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @ticket = @event.tickets.build(event_params)
+    if @ticket.save
+      redirect_to @event, notice: "「#{ @event.name }」に参加表明しました"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:ticket).permit(:text).merge(user_id: current_user.id)
+  end
 end
