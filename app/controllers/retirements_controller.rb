2374ad9663059000b7ac22c8d10b657eb394e040
kimu805 <kimukimuok805@gmail.com>
2024-12-06 18:15:16 +0900

    Update retirements_controller.rb

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -3,6 +3,9 @@ class RetirementsController < ApplicationController
   end
 
   def create
-    
+    if current_user.destroy
+      reset_session
+      redirect_to root_path, notice: "退会が完了しました。"
+    end
   end
 end
