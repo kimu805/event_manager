574a4d2ace4954e7c4955dd15f13d299b45b4089
kimu805 <kimukimuok805@gmail.com>
2024-12-06 18:31:50 +0900

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate
   def index
-    @events = Event.includes(:owner)
+    @events = Event.includes(:owner).future
   end
 end
