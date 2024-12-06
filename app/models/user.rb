fca5bde3c1c3cf865247f59f24f505d4e8fafdbf
kimu805 <kimukimuok805@gmail.com>
2024-12-06 18:19:13 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
+  before_destroy :check_all_events_finished
+
   # association
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未終了イベントが存在します"
+    end
+    
+  end
 end
