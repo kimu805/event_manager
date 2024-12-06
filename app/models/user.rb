ee8cea11df7103c94bbab9bf97126ccebff6192f
kimu805 <kimukimuok805@gmail.com>
2024-12-06 18:22:21 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # association
   has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
   has_many :tickets, dependent: :nullify
+  has_many :participating_events, thorough: :tickets, source: :event
 
   private
 
@@ -11,7 +12,10 @@ class User < ApplicationRecord
     now = Time.zone.now
     if created_events.where(":now < end_at", now: now).exists?
       errors[:base] << "公開中の未終了イベントが存在します"
+    elsif participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントが存在します"
     end
-    
+
+    throw(:abort) unless errors.empty?
   end
 end
