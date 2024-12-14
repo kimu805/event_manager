4b8942341536f9b7546840781bb77854dec7d821
kimu805 <kimukimuok805@gmail.com>
2024-12-14 15:45:06 +0900

    set up image culumn

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
 class Event < ApplicationRecord
+
+  attr_accessor :remove_image
+
   # association
   belongs_to :owner, class_name: "User"
   has_many :tickets, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :image, dependent: false
   
   # validation
   with_options presence:true do
@@ -18,12 +21,20 @@ class Event < ApplicationRecord
   scope :future, -> { where( ":now < start_at", now: Time.zone.now ) }
   scope :start_at_order, -> { order("start_at")}
 
+  # callback
+  before_save :remove_image_if_user_accept
+
   private
+
   def start_at_should_be_before_end_at
     return unless start_at && end_at
 
     if start_at >= end_at
       errors.add(:start_at, "は終了時間よりも前に設定してください")
     end
+  end
+
+  def remove_image_if_user_accept
+    self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
   end
 end
